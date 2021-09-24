f9761f2d68692216ce0654c02a7da38d425f044e
Gavin Loo <looweiren@gmail.com>
2021-09-25 01:05:07 +0800

    Edited crawler.py, finished scraping functions and enabled export of data to .csv file.

--- src/controller/crawler.py
+++ src/controller/crawler.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
+from selenium.common import exceptions
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.ie.options import ElementScrollBehavior
-from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import ElementNotInteractableException, NoSuchElementException
+from selenium.webdriver.remote.webelement import WebElement
 import time
+from datetime import datetime
+import csv
+import sys
 
 
 class Crawler:
@@ -30,6 +36,20 @@ class Crawler:
         '''
         time.sleep(seconds)
 
+    @staticmethod
+    def dateTime():
+        '''
+            Returns today's date and time
+            Parameters:
+                None
+            Returns:
+                None
+        '''
+        now = datetime.now()
+        datetime_string = now.strftime("%Y_%m_%d_%H_%M")
+
+        return datetime_string
+
     def navigate(self, location: str):
         '''
             Instructs crawler to Navigate to page.
@@ -39,6 +59,48 @@ class Crawler:
                 None
         '''
         pass
+
+    def moveToNextJob(self):
+        '''
+            Instructs crawler to move browser position to next job
+            Parameters:
+                None
+            Returns:
+                None
+        '''
+        body = self.driver.find_element_by_css_selector("body")
+
+        # Press the Down Button 4 times
+        for i in range(4):
+            body.send_keys(Keys.DOWN)
+
+    def moveErrorJob(self, previousSibling: WebElement, currentSibling: WebElement):
+        '''
+            Instructs crawler to move browser position back to previous job description, click it, then go back to current job description.
+            Parameters:
+                previousSibilng: WebElement => previous job description
+                currentSibiling: WebElement => current job description
+            Returns:
+                None
+        '''
+        body = self.driver.find_element_by_css_selector("body")
+
+        self.buffer(0.5)
+        # Press the Up button 4 times
+        for i in range(4):
+            body.send_keys(Keys.UP)
+
+        # Click the previous sibling element
+        previousSibling.click()
+
+        self.buffer(0.5)
+        # Press the Down button 4 times
+        for i in range(4):
+            body.send_keys(Keys.DOWN)
+
+        # Click the current sibling element
+        currentSibling.click()
+        self.buffer(0.5)
 
     def searchJobs(self, jobTitle: str, location: str):
         '''
@@ -124,68 +186,253 @@ class Crawler:
         self.buffer(0.5)
         doneButton.click()
 
-    def getJobInfo(self):
+    def getJobInfo(self, jobCount: int):
+        '''
+            Instructs crawler to scrape job description data
+            Parameters:
+                jobCount: int => Number of jobs descriptions to scrape
+            Returns:
+                None
+        '''
+
+        # Creates CSV File for storing data
+        with open(f"..\models\data\{self.dateTime()}_dataFile.csv", "w", encoding="utf-8", newline='') as csvFile:
+
+            # Initialize CSV DictWriter
+            csvColumns = ['jobTitle', 'companyName', 'location', 'datePosted', 'appStatus',
+                          'description', 'seniorityLevel', 'employmentType', 'jobFunction', 'industries']
+            writer = csv.DictWriter(csvFile, fieldnames=csvColumns)
+            writer.writeheader()
+
+            # Finds the <body> tag and <ul> tag for scraping.
+            body = self.driver.find_element_by_css_selector("body")
+            unorderedList = self.driver.find_element_by_css_selector(
+                "ul[class='jobs-search__results-list']")
+
+            # Moves the browser to location of first <li> tag
+            for i in range(6):
+                body.send_keys(Keys.DOWN)
+
+            # Finds the first <li> tag
+            currentLi = unorderedList.find_element_by_css_selector("li")
+            currentLi.click()
+            self.buffer(2)
+
+            # SCRAPE INFO
+            for i in range(jobCount):
+                # Moves browser in position for scraping
+                self.moveToNextJob()
+
+                # Finds the previous <li> tag
+                prevLi = currentLi
+
+                # Finds the next <li> tag
+                currentLi = currentLi.find_element_by_xpath(
+                    "following-sibling::*")
+
+                currentLi.click()
+                self.buffer(1)
+                currentLi.click()
+                self.buffer(1)
+
+                # Check whether <li> was clicked and data is showing
+                for i in range(5):
+                    # Attempt to fix job description not showing bug
+                    if not self.tryToFindData():
+                        self.moveErrorJob(prevLi, currentLi)
+
+                    else:
+                        break
+
+                # Get data from LinkedIn
+                data = self.getJobData()
+
+                # Append data to CSV File
+                writer.writerow(data)
+
+                # Finds the see more <button>, then clicks on it. Allows crawler to scrape more job descriptions.
+                try:
+                    seeMoreButton = unorderedList.find_element_by_xpath(
+                        "..").find_element_by_css_selector("button[aria-label='Load more results']")
+                    seeMoreButton.click()
+                except ElementNotInteractableException:
+                    pass
+
+            # Close CSV File after writing to prevent memory leakage
+            csvFile.close()
+
+    def getJobData(self):
         '''
             Instructs crawler to scrape job description data
             Parameters:
                 None
             Returns:
-                None
+                dictionary => dictionary keys: dataType, dictionary values: dataValues
         '''
-        # Finds the <body> tag and <ul> tag for scraping.
-        body = self.driver.find_element_by_css_selector("body")
-        unorderedList = self.driver.find_element_by_css_selector(
-            "ul[class='jobs-search__results-list']")
+        # Find the Show More <button>, If it exists, click it
+        try:
+            showMoreButton = self.driver.find_element_by_css_selector(
+                "button[aria-label= 'Show more, visually expands previously read content above this button']")
+            showMoreButton.click()
+            self.buffer(2)
+        except NoSuchElementException:
+            pass
 
-        # Moves the browser to location of first <li> tag
-        for i in range(6):
-            body.send_keys(Keys.DOWN)
+        # Find the content <div> for job description information
+        try:
+            contentDiv = self.driver.find_element_by_css_selector(
+                "div[class='details-pane__content details-pane__content--show']")
 
-        # Finds the first <li> tag
-        currentLi = unorderedList.find_element_by_css_selector("li")
-        currentLi.click()
-        self.buffer(2)
+        except NoSuchElementException:
+            return
 
-        # SCRAPE INFO
-        for i in range(1000):
-            # Moves browser in position for scraping
-            self.moveToNextJob()
+        jobTitle = self.scrapeJobTitle(contentDiv)
+        companyName = self.scrapeCompanyName(contentDiv)
+        location = self.scrapeLocation(contentDiv)
+        datePosted = self.scrapeDatePosted(contentDiv)
+        appStatus = self.scrapeApplicantsStatus(contentDiv)
+        description = self.scrapeDescription(contentDiv)
 
-            # Finds the next <li> tag
-            currentLi = currentLi.find_element_by_xpath(
-                "following-sibling::*")
-            currentLi.click()
-            self.buffer(1)
-            currentLi.click()
-            self.buffer(1)
+        seniorityLevel = self.scrapeSeniorityLevel(contentDiv)
+        employmentType = self.scrapeEmploymentType(contentDiv)
+        jobFunction = self.scrapeJobFunction(contentDiv)
+        industries = self.scrapeIndustries(contentDiv)
 
-            # Scrape some data
-            # ....................
+        print(description)
+        dataDictionary = {
+            "jobTitle": jobTitle,
+            "companyName": companyName,
+            "location": location,
+            "datePosted": datePosted,
+            "appStatus": appStatus,
+            "description": description,
+            "seniorityLevel": seniorityLevel,
+            "employmentType": employmentType,
+            "jobFunction": jobFunction,
+            "industries": industries,
+        }
 
-            # Finds the see more <button>, then clicks on it. Allows crawler to scrape more job descriptions.
-            try:
-                seeMoreButton = unorderedList.find_element_by_xpath(
-                    "..").find_element_by_css_selector("button[aria-label='Load more results']")
-                seeMoreButton.click()
-            except ElementNotInteractableException:
-                pass
+        return dataDictionary
 
-    def selectJob(self):
-        pass
-
-    def moveToNextJob(self):
+    def tryToFindData(self):
         '''
-            Instructs crawler to move browser position to next job
+            Instructs crawler to try to find job description data
             Parameters:
                 None
             Returns:
-                None
+                bool => True if data is found, False if data is not found
         '''
-        body = self.driver.find_element_by_css_selector("body")
+        # Try to find Job Title <h2>
+        h2 = self.driver.find_element_by_css_selector(
+            "h2[class='top-card-layout__title topcard__title']").text
 
-        # Press the Down Button 4 times
-        for i in range(4):
-            body.send_keys(Keys.DOWN)
+        return not(h2 == "")
+
+    def scrapeJobTitle(self, currentDiv: WebElement):
+        try:
+            jobTitle = currentDiv.find_element_by_css_selector(
+                "h2[class='top-card-layout__title topcard__title']").text
+            return jobTitle.strip().replace("\n", " ")
+
+        except NoSuchElementException:
+            return ""
+
+    def scrapeCompanyName(self, currentDiv: WebElement):
+        try:
+            companyName = currentDiv.find_element_by_css_selector(
+                "a[data-tracking-control-name='public_jobs_topcard-org-name']").text
+            return companyName.strip().replace("\n", " ")
+
+        except NoSuchElementException:
+            return ""
+
+    def scrapeLocation(self, currentDiv: WebElement):
+        try:
+            location = currentDiv.find_element_by_css_selector(
+                "span[class='topcard__flavor topcard__flavor--bullet']").text
+            return location.strip().replace("\n", " ")
+
+        except NoSuchElementException:
+            return ""
+
+    def scrapeDatePosted(self, currentDiv: WebElement):
+        try:
+            datePosted = currentDiv.find_element_by_css_selector(
+                "span[class='posted-time-ago__text topcard__flavor--metadata']").text
+            return datePosted.strip().replace("\n", " ")
+
+        except NoSuchElementException:
+            return ""
+
+    def scrapeApplicantsStatus(self, currentDiv: WebElement):
+        try:
+            applicantsStatus = currentDiv.find_element_by_css_selector(
+                "figcaption[class='num-applicants__caption']").text
+            return applicantsStatus.strip().replace("\n", " ")
+
+        except NoSuchElementException:
+            return ""
+
+    def scrapeDescription(self, currentDiv: WebElement):
+        try:
+            description = currentDiv.find_element_by_css_selector(
+                "div[class='show-more-less-html__markup']").text
+            return description.strip().replace("\n", " ")
+
+        except NoSuchElementException:
+            return ""
+
+    def scrapeSeniorityLevel(self, currentDiv: WebElement):
+        try:
+            unorderedList = currentDiv.find_element_by_css_selector(
+                "ul[class='description__job-criteria-list']").find_elements_by_css_selector("li")
+            for element in unorderedList:
+                label = element.find_element_by_css_selector("h3")
+                data = element.find_element_by_css_selector("span")
+                if "Seniority level" in label.text:
+                    return data.text.strip().replace("\n", " ")
+
+        except NoSuchElementException:
+            return ""
+
+    def scrapeEmploymentType(self, currentDiv: WebElement):
+        try:
+            unorderedList = currentDiv.find_element_by_css_selector(
+                "ul[class='description__job-criteria-list']").find_elements_by_css_selector("li")
+            for element in unorderedList:
+                label = element.find_element_by_css_selector("h3")
+                data = element.find_element_by_css_selector("span")
+                if "Employment type" in label.text:
+                    return data.text.strip().replace("\n", " ")
+
+        except NoSuchElementException:
+            return ""
+
+    def scrapeJobFunction(self, currentDiv: WebElement):
+        try:
+            unorderedList = currentDiv.find_element_by_css_selector(
+                "ul[class='description__job-criteria-list']").find_elements_by_css_selector("li")
+            for element in unorderedList:
+                label = element.find_element_by_css_selector("h3")
+                data = element.find_element_by_css_selector("span")
+                if "Job function" in label.text:
+                    return data.text.strip().replace("\n", " ")
+
+        except NoSuchElementException:
+            return ""
+
+    def scrapeIndustries(self, currentDiv: WebElement):
+        try:
+            unorderedList = currentDiv.find_element_by_css_selector(
+                "ul[class='description__job-criteria-list']").find_elements_by_css_selector("li")
+            for element in unorderedList:
+                label = element.find_element_by_css_selector("h3")
+                data = element.find_element_by_css_selector("span")
+                if "Industries" in label.text:
+                    return data.text.strip().replace("\n", " ")
+
+        except NoSuchElementException:
+            return ""
 
     def exitCrawler(self):
         '''
@@ -202,4 +449,4 @@ class Crawler:
 if __name__ == "__main__":
     myCrawler = Crawler()
     myCrawler.searchJobs("Sales", "Singapore")
-    myCrawler.getJobInfo()
+    myCrawler.getJobInfo(5)
