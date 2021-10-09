df6faf9148270ec0e0d8ed4febabc4608affa12c
Wangu <2101287@sit.singaporetech.edu.sg>
2021-10-09 18:52:22 +0800

    some cleaned Russia data and err pandagui is there for u to try out

--- src/views/graphicUI.py
+++ src/views/graphicUI.py
@@ -1,10 +1,17 @@
 import sys
+import pandas as pd
+from pandasgui import show
+
+
 sys.path.append("../")
 
 
-def importMods():
-    from controller import cleaner
+def toshowalldata():
+    ds = pd.read_csv("../data/cleanedData/Russia/All/2021_10_08_23_43_Sales_dataFile.csv")
+    show(ds)
 
 
-importMods()
-print("cleaner")
+
+
+toshowalldata()
+
