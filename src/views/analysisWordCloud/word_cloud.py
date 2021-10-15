4da5a9ce623c13f36f54489ef1a36fbd4588638a
Tan Jia Jia, Serene <kei.no.melonpan@gmail.com>
2021-10-15 12:21:45 +0800

    Added word_cloud.py (tbc)

---
+++ src/views/analysisWordCloud/word_cloud.py
@@ -0,0 +1,34 @@
+from tkinter import Label
+import pandas as pd
+import numpy as np
+from wordcloud import (WordCloud, get_single_color_func)
+from PIL import Image
+import matplotlib.pyplot as plt
+# % matplotlib inline
+
+
+# Dataset (WordCloud)
+datasetDataframe = pd.read_csv("../../data/keywords/KEYWORDS_AllCountries_All_Data.csv")
+
+df_keyword = []
+df_count =[]
+df_type = []
+df_keyword = datasetDataframe.get(key='keyword').tolist()
+
+df_count = datasetDataframe.get(key='count').tolist()
+df_type = datasetDataframe.get(key='type_').tolist()
+
+
+df_dict = dict(zip(df_keyword,df_count))
+# print(df_dict)
+
+wordcloud = WordCloud(background_color='white',
+                      width=1500,
+                      height=1000
+                      ).generate_from_frequencies(df_dict)
+# use .generate(space_separated_string) - to generate cloud from text
+
+plt.figure(figsize=(15,13))
+plt.imshow(wordcloud, interpolation='bilInear')
+plt.axis('off')
+plt.show()
--- src/views/analysisWordCloud/wordcloud.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-
-# Dataset (WordCloud)
-datasetDataframe = pd.read_csv("../../data/keywords/KEYWORDS_AllCountries_All_Data.csv")
